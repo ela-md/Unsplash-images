8e7b243da48a1e88537928625d7572b23bae4f4f
ela.md <ela.ahmadi06@gmail.com>
2025-04-04 13:51:17 -0400

    Search Form Created

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,11 +1,30 @@
 
 function SearchForm () {
 
+    const handleSubmit = (e) => {
+       e.preventDefault()
+       const searchValue = e.target.elements.search.value
+       if(!searchValue) return;
+
+    }
+
+    
+
+    
 
     return(
-     <>
-     <h1>search form</h1>
-     </>
+     <section>
+        <h1 className="title">unsplash images</h1>
+        <form className="search-form" onSubmit={handleSubmit}>
+            <input type="text"
+             className="form-input search-input"
+            name="search" 
+            placeholder="cat"/>
+            <button type="submit" className="btn">
+              search
+            </button>
+        </form>
+     </section>
     );
 }
 
