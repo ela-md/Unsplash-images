d63c793885b435db3f13d36380a0fc0263419fb1
ela.md <ela.ahmadi06@gmail.com>
2025-04-05 20:15:34 -0400

    render images

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -15,11 +15,45 @@ function Gallery () {
       },
     });
 
-   return(
-    <>
-     <h2>Gallery</h2>
-    </>
-   )
+    if (response.isLoading){
+      return (
+        <section>
+            <h4>Loading...</h4>
+        </section>
+      )
+  }
+
+  if (response.isError){
+      return (
+        <section>
+            <h4>There was an error...</h4>
+        </section>
+      )
+  }
+
+  const results = response.data.results
+ if(results.lenght < 1)
+  <section>
+      <h4>No results found...</h4>
+  </section>
+  
+  
+
+  return(
+     <section className="imahe-container">
+       {results.map((item)=>{
+          const url = item?.urls?.regular
+          return  <img
+          src={url}
+          key={item.id}
+          alt={item.alt_description}
+          className="img"
+          ></img>
+       })}
+     </section>
+   
+
+  );
 };
 
 export default Gallery 
